fa067752b5bdfcf4b42b281c343b90852b8cca30
vahbib <singhbibhav084@gmail.com>
2020-09-27 16:08:54 +0530

    Added Main Class File

---
+++ LineComparisonOOPs_Main.java
@@ -0,0 +1,16 @@
+package LineComparisonOOPs;
+
+public class LineComparisonOOPs_Main {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		LineComparisonOOPs LObj = new LineComparisonOOPs();
+		
+		LObj.inputs();
+		LObj.Calculation();
+		LObj.CompareMethod();
+	}
+
+}
